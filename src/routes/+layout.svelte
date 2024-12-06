9d13f462bcae29d1283e60b5e773bae5403b2e72
trasherdk <trasherdk@gmail.com>
2024-12-06 04:42:45 +0100

    Add +layout and app.css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+	import '../app.css';
+	/**
+	 * @typedef {Object} Props
+	 * @property {import('svelte').Snippet} [children]
+	 */
+
+	/** @type {Props} */
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<header>
+		<div class="logo-container">
+			<div class="logo"></div>
+			<span class="website-name">XMR Sample (Svelte)</span>
+		</div>
+	</header>
+
+	<main>
+		{@render children?.()}
+	</main>
+
+	<footer>
+		<p>© 2024 TrasherDK</p>
+	</footer>
+</div>
+
+<style>
+	/* Empty */
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+	import Qrcode from '@trasherdk/svelte-qrcode';
+</script>
+
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Qrcode content="@trasherdk/svelte-qrcode" />
