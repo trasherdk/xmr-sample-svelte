a8573090152a563cdce2e099a43223d82e37ce38
trasherdk <trasherdk@gmail.com>
2025-06-12 10:29:59 +0200

    Move brand into component - Logo & App title

---
+++ src/lib/components/Brand.svelte
@@ -0,0 +1,26 @@
+<brand>
+	<logo></logo>
+	<websitename>XMR Sample (Svelte)</websitename>
+</brand>
+
+<style>
+	brand {
+		display: flex;
+		background-color: transparent;
+		align-items: center;
+		gap: 1rem;
+		margin-right: 2rem;
+	}
+
+	websitename {
+		font-size: 1.5rem;
+		font-weight: bold;
+	}
+
+	logo {
+		height: 40px;
+		width: 40px;
+		margin-right: 0.5rem;
+		/* Placeholder for the actual logo */
+	}
+</style>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
+	import Brand from './Brand.svelte';
 	import Navbar from './Navbar.svelte';
 	import User from './User.svelte';
 	import Theme from './Theme.svelte';
 </script>
 
 <header>
-	<brand>
-		<logo></logo>
-		<websitename>XMR Sample (Svelte)</websitename>
-	</brand>
-	<Navbar />
-	<usercontainer>
+	<section>
+		<Brand />
+		<Navbar />
+	</section>
+	<section>
 		<User />
 		<Theme />
-	</usercontainer>
+	</section>
 </header>
 
 <style>
@@ -25,27 +25,7 @@
 		background-color: var(--surface-1);
 	}
 
-	brand {
-		display: flex;
-		background-color: transparent;
-		align-items: center;
-		gap: 1rem;
-		margin-right: 2rem;
-	}
-
-	websitename {
-		font-size: 1.5rem;
-		font-weight: bold;
-	}
-
-	logo {
-		height: 40px;
-		width: 40px;
-		margin-right: 0.5rem;
-		/* Placeholder for the actual logo */
-	}
-
-	usercontainer {
+	section {
 		display: flex;
 		align-items: center;
 		gap: 1rem;
