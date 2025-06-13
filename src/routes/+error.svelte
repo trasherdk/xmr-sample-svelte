d9797cf4aa39e5b8821d0fc46ff1f9d1d797ce79
trasherdk <trasherdk@gmail.com>
2025-06-13 07:06:04 +0200

    Fix +error.svelte theme

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,10 +2,23 @@
 	import { page } from '$app/state';
 </script>
 
-<h1>{page.status} {page.error?.message || 'An error occurred'}</h1>
+<div class="error-container">
+	<h1>{page.status} {page.error?.message || 'An error occurred'}</h1>
+</div>
 
 <style>
-	h1 {
+	.error-container {
+		display: flex;
+		flex-direction: column;
+		min-height: 50vh;
+		align-items: center;
+		justify-content: center;
+		background-color: var(--surface-1);
+		color: var(--text-1);
+		padding: 2rem;
+	}
+
+	.error-container h1 {
 		margin: auto;
 		color: var(--text-3);
 		max-width: fit-content;
